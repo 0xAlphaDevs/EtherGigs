506367517e47487670876f4bfc13644964929643
0xyshv <writetoyshv@gmail.com>
2024-04-20 18:08:59 +0530

    chore: add type schemas

---
+++ src/lib/types.ts
@@ -0,0 +1,34 @@
+export type User = {
+  name: string;
+  location: string;
+  userType: string;
+  rating: number;
+  amountSpent: number;
+};
+
+export type Job = {
+  jobId: string;
+  title: string;
+  description: string;
+  status:
+    | "active"
+    | "ongoing"
+    | "completedbygigster"
+    | "approvedbyclient"
+    | "closed";
+  tags: string[];
+  budget: number;
+  proposals: number;
+  createdAt: string;
+  createdBy: string;
+};
+
+export type Proposal = {
+  proposalId: string;
+  jobId: string;
+  status: "pending" | "accepted" | "rejected";
+  bid: number;
+  description: string;
+  createdAt: string;
+  createdBy: string;
+};
