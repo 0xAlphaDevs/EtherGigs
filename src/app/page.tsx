35a219caadd380e9c37b94610fb76394528d7802
0xyshv <writetoyshv@gmail.com>
2024-04-20 08:34:37 +0530

    chore: add feature card and footer for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import HomeNavbar from '@/components/home/navbar';
 import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 export default function Home() {
   return (
     <>
       <HomeNavbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center gap-12 justify-between p-24">
         <div className='flex flex-col gap-4 text-center'>
           <p className='text-5xl font-bold'> Welcome to EtherGigs</p>
           <p className="text-2xl text-center">
@@ -13,7 +22,47 @@ export default function Home() {
             professionals
           </p>
         </div>
-      </main>
+        <div>
+          {/* TO DO :  Login functionality will be here */}
+        </div>
+        {/* Features card div */}
+        <div className="grid grid-cols-3 gap-8">
+          <Card className="bg-opacity-65 shadow-lg cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
+            <CardHeader>
+              <CardTitle className="text-center">Decentralized</CardTitle>
+              <CardDescription className="text-center">
+                EtherGigs is a decentralized freelancing platform connecting
+                businesses and professionals
+              </CardDescription>
+            </CardHeader>
+          </Card>
+
+          <Card className="bg-opacity-65 shadow-lg cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
+            <CardHeader>
+              <CardTitle className="text-center"></CardTitle>
+              <CardDescription className="text-center">
+
+              </CardDescription>
+            </CardHeader>
+          </Card>
+
+          <Card className="bg-opacity-65 shadow-lg cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
+            <CardHeader>
+              <CardTitle className="text-center"></CardTitle>
+              <CardDescription className="text-center">
+
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+        {/* Footer */}
+        <div className="fixed container mx-auto bottom-0 pb-4 ">
+          <hr className="border-t-2 border-gray-600 dark:border-white mb-4" />
+          <div className="flex justify-center items-center">
+            <p>&copy; 2024 EtherGigs | All rights reserved</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
