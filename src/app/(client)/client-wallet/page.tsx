2c17495875646c4fad19ef0303dd95e09c248e5a
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-04-22 14:15:57 +0530

    feat: add xtz balances

--- src/app/(client)/client-wallet/page.tsx
+++ src/app/(client)/client-wallet/page.tsx
@@ -1,17 +1,31 @@
-
-
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 import { MapPin } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useBalance, useAccount } from "wagmi";
 
 const ClientWallet = () => {
+  const { address } = useAccount();
+  // const [balance, setBalance] = useState<any>(0.0);
+
+  const result = useBalance({
+    address: address,
+  });
+  console.log(result.data);
+
+  // useEffect(() => {
+  //   setBalance(result.data?.value || 0.0);
+  // }, [result]);
+
+  // console.log(balance);
+
   return (
     <div className="flex flex-col gap-10 p-16">
-      <div className='flex flex-col items-center gap-8 pt-8'>
-        <p className='font-semibold text-6xl'>Hello, XYZ</p>
-        <div className='flex gap-4'>
+      <div className="flex flex-col items-center gap-8 pt-8">
+        <p className="font-semibold text-6xl">Hello, XYZ</p>
+        <div className="flex gap-4">
           <MapPin />
-          <p>Location</p>
+          <p>On-Chain</p>
         </div>
       </div>
       <Card className=" bg-opacity-65 shadow-md mx-72">
@@ -21,11 +35,13 @@ const ClientWallet = () => {
         </CardHeader>
         <CardContent className="flex flex-col gap-8 items-center">
           <p className="font-bold text-lg">Etherlink Testnet</p>
-          <div>XTZ Balance : </div>
+          <div>
+            Your XTZ Balance : {result.data?.formatted} {result.data?.symbol}{" "}
+          </div>
         </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default ClientWallet
+export default ClientWallet;
